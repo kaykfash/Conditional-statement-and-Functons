39f8f36517ce8712df85ecf74b75643d1f46de9c
Kehinde Fashogbon <Kkoladefash@yahoo.com>
2021-03-14 12:20:45 +0100

    Class classcorrection

---
+++ classcorrection.js
@@ -0,0 +1,18 @@
+
+function doMath(numberOne,numberTwo,string){
+    if(string ==="sum"){
+        const additionOfNumbers= numberOne + numberTwo
+      return  additionOfNumbers
+    }
+    
+   if(string ==="divide"){
+        const divisionOfNumbers= numberOne/numberTwo;
+        return divisionOfNumbers
+    }
+  
+     if(string==="multiply"){
+        const productOfNumbers= numberOne* numberTwo;
+        return productOfNumbers
+    }
+    
+  };
