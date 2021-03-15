3b6a98e8835822cde5a3f2aa5bee911e9787b2fd
Kehinde Fashogbon <Kkoladefash@yahoo.com>
2021-03-15 09:01:31 +0100

    Function Assignment

--- correction.js
+++ correction.js
@@ -15,32 +15,6 @@ function funNumbers(valueOne,valueTwo,char){
     
   };
 
-
-
-  // Function that gives the length of a text
-
-  function displayTextLength(string){
-    return string.length;
-}
-
-//  displayTextLength('Kehinde');
-console.log(displayTextLength('Ebenezer'))
-
-
-// Function that gives the square of a number
-function calculateSquare(number){
-  return number * number;
-}
-calculateSquare(9)
-
-// Function that converts text to uppercase
-
-function changeToUppercase(string){
-return string.toUpperCase();
-}
-
-changeToUppercase('i love this shit !')
-  
    console.log(funNumbers(5,6,"sum"));
     console.log(funNumbers(5,6,"divide"));
     console.log(funNumbers(5,6,"multiply"));
---
+++ function.js
@@ -0,0 +1,26 @@
+// Function that gives the length of a text
+
+function displayTextLength(string){
+    return string.length;
+}
+
+ displayTextLength('Kehinde');
+console.log(displayTextLength('Kehinde'))
+
+
+// Function that gives the square of a number
+function calculateSquare(number){
+  return number * number;
+}
+calculateSquare(9)
+console.log(calculateSquare(9))
+
+// Function that converts text to uppercase
+
+function changeToUpperCase(string){
+return string.toUpperCase();
+}
+
+changeToUpperCase('i am getting a hold of this !')
+console.log(changeToUpperCase('i am getting a hold of this !'))
+  
